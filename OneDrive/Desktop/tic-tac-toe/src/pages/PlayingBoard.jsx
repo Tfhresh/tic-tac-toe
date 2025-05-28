b716e9f72dbfe7c2469777ba33e2a9ec0c87cef5
Temiloluwa Adebayo <temiadebayo15.com>
2025-05-28 15:09:56 +0100

    Another commit

--- OneDrive/Desktop/tic-tac-toe/src/pages/PlayingBoard.jsx
+++ OneDrive/Desktop/tic-tac-toe/src/pages/PlayingBoard.jsx
@@ -1,59 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import PlayerCard from '../components/PlayerCard'
 import GameBoard from '../components/GameBoard'
-// import { playersConstant } from '../constant/players';
-
-
-
 
 const PlayingBoard = () => {
 
-  const [playerTurn, setPlayerTurn] = useState(1);
-
-  const [boardBoxes, setBoardBoxes] = useState([-1, -1, -1, -1, -1, -1, -1, -1, -1]);
+  const [playerTurn, setPlayerTurn] = useState(0);
+  const [players, setPlayers] = useState([
+    {
+      player: 1,
+      score: 0
+    },
+    {
+      player: 2,
+      score: 0
+    }
+  ]);
+  const [boardBoxes, setBoardBoxes] = useState(Array.from(new Array(9)).map(() => undefined));
 
   const handlePlay = (index) => {
     setBoardBoxes(prevBoxes => {
-      // const boxIndex = prevBoxes.find(el => el.index === index);
-      // if (boxIndex) {
-      //   return prevBoxes;
-      // }
-      // setPlayerTurn(playerTurn === playersConstant.PLAYER1 ? playersConstant.PLAYER2 : playersConstant.PLAYER1);
-      // return [...prevBoxes, { player: playerTurn, index }];
-
-      if (prevBoxes[index] === -1) {
+      if (prevBoxes[index] === undefined) {
         prevBoxes[index] = playerTurn;
+        setPlayerTurn(playerTurn === 0 ? 1 : 0);
         return [...prevBoxes];
       }
       return prevBoxes;
     });
-    setPlayerTurn(() => playerTurn === 0 ? 1 : 0);
   };
 
+  useEffect(() => {
+     const checkWin = () => {
+      const conditionCheck = (i1, i2, i3) =>
+      (boardBoxes[i1] === 0 && boardBoxes[i2] === 0 && boardBoxes[i3] === 0) ||
+      (boardBoxes[i1] === 1 && boardBoxes[i2] === 1 && boardBoxes[i3] === 1);
 
-  const conditionCheck = (i1, i2, i3) => (boardBoxes[i1] === 0 && boardBoxes[i2] === 0 && boardBoxes[i3] === 0) ||
-    (boardBoxes[i1] === 1 && boardBoxes[i2] === 1 && boardBoxes[i3] === 1)
-    ;
+      if (conditionCheck(0, 1, 2) ||
+        conditionCheck(3, 4, 5) ||
+        conditionCheck(6, 7, 8) ||
+        conditionCheck(0, 3, 6) ||
+        conditionCheck(1, 4, 7) ||
+        conditionCheck(2, 5, 8) ||
+        conditionCheck(0, 4, 8) ||
+        conditionCheck(2, 4, 6)
+      ) {
+        setPlayers(prevPlayers=> {
+          prevPlayers[playerTurn] = {
+            ...prevPlayers[playerTurn],
+            score: prevPlayers[playerTurn].score + 1
+          } 
+          return [...prevPlayers];
+        });
+        setTimeout(()=> {
+          setBoardBoxes([...Array.from(new Array(9)).map(() => undefined)]);
+        }, 1000);
+      }
+    };
 
+    checkWin();
+  }, [boardBoxes, playerTurn]);
 
-  const checkWin = () => {
-
-    // console.log(boardBoxes[0] === boardBoxes[1] === boardBoxes[2]);
-
-    if (conditionCheck(0, 1, 2) ||
-      conditionCheck(3, 4, 5) ||
-      conditionCheck(6, 7, 8) ||
-      conditionCheck(0, 3, 6) ||
-      conditionCheck(1, 4, 7) ||
-      conditionCheck(2, 5, 8) ||
-      conditionCheck(0, 4, 8) ||
-      conditionCheck(2, 4, 6)
-    ) {
-      alert(`player${playerTurn} wins!!!`);
-    }
-  };
-
-  checkWin();
 
   return (
     <div className='w-[375px] h-[calc(100vh-40px)] max-h-[667px] bg-[#203364] rounded-2xl'>
@@ -61,13 +66,14 @@ const PlayingBoard = () => {
         <h4 className='text-xs text-gray-100 font-medium'>Players Mode</h4>
       </div>
       <div className='flex items-center justify-between px-5'>
-        <PlayerCard playerName="Player 1:" playerScore={0} />
-        <PlayerCard playerName="Player 2:" playerScore={1} />
+        <PlayerCard playerName={`Player ${players[0].player}`} playerScore={players[0].score} />
+        <PlayerCard playerName={`Player ${players[1].player}`} playerScore={players[1].score} />
       </div>
       <div className='text-center mt-4 mb-8'>
-        <p className='text-sm font-extrabold text-white'>Player 1 Turn</p>
+        <p className='text-sm font-extrabold text-white'>Player {players[playerTurn].player} Turn</p>
       </div>
       <div className='px-5'>
+        {/* <button></button> */}
         <GameBoard playedBoardBoxes={boardBoxes} onPlayerPlay={handlePlay} />
       </div>
     </div>
